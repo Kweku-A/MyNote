267b46e64b6bae975a1c5bd8602fdb2a20113a8a
KwekuArmah <karmah@aligntech.com>
2022-11-19 23:42:12 +0100

    Create CoroutinesModule.kt

---
+++ app/src/main/java/com/kweku/armah/mynote/di/CoroutinesModule.kt
@@ -0,0 +1,18 @@
+package com.kweku.armah.mynote.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object CoroutinesModule {
+
+    @Provides
+    @Singleton
+    fun providesIODispatcher(): CoroutineDispatcher = Dispatchers.IO
+}
